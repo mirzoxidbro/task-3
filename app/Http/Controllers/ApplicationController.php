853c1e5fe21ec9fc9c39acd520f3fe4d5c380388
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2022-10-26 10:37:09 +0300

    store

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Application;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        if($request->hasFile('file'))
+        {
+            $fileName = $request->file('file')->getClientOriginalName();
+            $path = $request->file('file')->storeAs('files', $fileName, 'public');
+        }
+
+        $request->validate([
+            'subject' => 'required|max:255',
+            'message' => 'required',
+            'file' => 'file|mimes:png,jpg,pdf'
+        ]);
+
+
+        Application::create([
+            'user_id' => $request->user_id,
+            'subject' => $request->subject,
+            'message' => $request->message,
+            'file_url' => $path ?? null
+        ]);
+
+        return redirect()->back();
+
+
+    }
 }
